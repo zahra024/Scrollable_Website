923305c487009c036b9328d88185e1a316aee807
zahra024 <zahrafatimanida06@gmail.com>
2024-12-09 21:25:51 +0530

    Scrollable Website

---
+++ index.js
@@ -0,0 +1,16 @@
+function scrollToAboutUs() {
+  const aboutUsDiv = document.getElementById("about-us");
+  aboutUsDiv.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToCampus() {
+  const campusDiv = document.getElementById("campus");
+  campusDiv.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToculture() {
+  const cultureDiv = document.getElementById("culture");
+  cultureDiv.scrollIntoView({ behavior: "smooth" });
+}
+function scrollTocontact() {
+  const contactDiv = document.getElementById("contact");
+  contactDiv.scrollIntoView({ behavior: "smooth" });
+}
